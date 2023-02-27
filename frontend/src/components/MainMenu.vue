a4c9a231f924048f9b034202e6de66965700edb1
Zamoca <suntail2002@naver.com>
2023-02-28 00:58:06 +0900

    design: Modify UI of the main menu

--- frontend/src/components/MainMenu.vue
+++ frontend/src/components/MainMenu.vue
@@ -1,69 +1,96 @@
 <template>
-    <v-app-bar color="indigo-lighten-5" density="compact" class="justify-space-between">
+  <v-app-bar
+    color="indigo-lighten-5"
+    density="compact"
+    class="justify-space-between"
+  >
+    <div class="ma-1 pa-1 me-auto">zamoca.space</div>
 
-        <v-app-bar-nav-icon class="d-flex d-sm-none" @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon
+      icon="mdi-vuetify"
+      class="d-flex d-sm-none ma-2 pa-2 me-auto"
+      @click="drawer = !drawer"
+    >
+    </v-app-bar-nav-icon>
+    <div class="d-none d-sm-flex me-auto">
+    <v-btn
+      variant="text"
+      v-for="(item, i) in items"
+      :key="i"
+      :value="item"
+      :href="item.href"
+    >
+      {{ item.text }}
+    </v-btn>
+    </div>
+    <!-- <v-btn variant="text" href="/blog/post_list">Blog</v-btn> -->
+    <v-btn href="/"><v-icon>mdi-magnify</v-icon>Tag</v-btn>
+  </v-app-bar>
 
-        <v-toolbar-title>zamoca.space</v-toolbar-title>
+  <v-navigation-drawer
+    :elevation="1"
+    color="indigo-lighten-5"
+    class="d-flex d-sm-none"
+    v-model="drawer"
+    location="top"
+    rail="true"
+    :rail-width="railwidth"
+  >
+    <v-list density="compact">
+      <!-- <v-list-subheader>menu</v-list-subheader> -->
+      <v-list-item
+        class="justify-center"
+        v-for="(item, i) in items"
+        :key="i"
+        :value="item"
+        :href="item.href"
+        active-color="primary"
+      >
+        <template v-slot:prepend>
+          <!-- <v-icon :icon="item.icon"></v-icon> -->
+        </template>
 
-        <span class="d-none d-sm-flex">
-            <v-btn variant="text" v-for="(item, i) in items" :key="i" :value="item" :href="item.href" >{{ item.text }}</v-btn>
-            <!-- <v-btn variant="text" href="/blog/post_list">Blog</v-btn> -->
-        </span>
-
-        <v-spacer></v-spacer>
-
-        <span>
-            <v-btn href="/"><v-icon>mdi-magnify</v-icon>Tag</v-btn>
-        </span>
-
-    </v-app-bar>
-
-    <v-navigation-drawer class="d-flex d-sm-none" v-model="drawer" app clipped>
-        <v-list density="compact">
-            <v-list-subheader>menu</v-list-subheader>
-
-            <v-list-item v-for="(item, i) in items" :key="i" :value="item" :href="item.href" active-color="primary">
-                <template v-slot:prepend>
-                    <!-- <v-icon :icon="item.icon"></v-icon> -->
-                </template>
-
-                <v-list-item-title v-text="item.text"></v-list-item-title>
-            </v-list-item>
-        </v-list>
-    </v-navigation-drawer>
+        <v-list-item-title v-text="item.text"></v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
-  
-<script>
-import axios from 'axios';
-import { user } from './globals.js';
 
-axios.defaults.xsrfCookieName = 'csrftoken';
-axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+<script>
+import axios from "axios";
+import { user } from "./globals.js";
+
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
 export default {
-    setup() {
-        return { user }
-    },
+  setup() {
+    return { user };
+  },
 
-    data: () => ({
-        drawer: null,
-        items: [
-            { text: 'Info', href: '/' },
-            { text: 'Blog', href: '/blog/post_list.html' },
-        ],
-    }),
+  data: () => ({
+    drawer: null,
+    items: [
+      { text: "Info", href: "/" },
+      { text: "Blog", href: "/blog/post_list.html" },
+    ],
+  }),
 
-    created() {
-        // console.log("created(MainMenu.vue)...");
-        // this.user.getUserInfo();
-    },
+  computed: {
+    railwidth() {
+        return 56 * this.items.length
+    }
+  },
 
-    mounted() {
-        // console.log("mounted()...");
-    },
+  created() {
+    // console.log("created(MainMenu.vue)...");
+    // this.user.getUserInfo();
+  },
 
-    methods: {
+  mounted() {
+    // console.log("mounted()...");
+  },
 
-    },
-}
+  methods: {},
+};
 </script>
