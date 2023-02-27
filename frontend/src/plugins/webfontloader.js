0fdc66d6585febe0c5d48fd863e2c0958dbdd49e
Zamoca <suntail2002@naver.com>
2023-02-27 23:02:49 +0900

    refactor: Replace font with Pretendard in plugin

--- frontend/src/plugins/webfontloader.js
+++ frontend/src/plugins/webfontloader.js
@@ -8,8 +8,13 @@
   const webFontLoader = await import(/* webpackChunkName: "webfontloader" */'webfontloader')
 
   webFontLoader.load({
-    google: {
-      families: ['Roboto:100,300,400,500,700,900&display=swap'],
+    // google: {
+    //   families: ['Roboto:100,300,400,500,700,900&display=swap'],
+    //   families: ['Pretendard:100,300,400,500,700,900&display=swap'],
+    // },
+    custom: {
+      families: ['"Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif'],
+      urls: ['https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard.css']
     },
   })
 }
