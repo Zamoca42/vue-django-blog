5d0bbc15e1e8d9c9e69014dd16a703b6ba23298d
Zamoca <suntail2002@naver.com>
2023-03-01 21:20:48 +0900

    design: modify Postlist layout

--- frontend/src/components/PostList.vue
+++ frontend/src/components/PostList.vue
@@ -1,91 +1,59 @@
 <template>
-  <v-container>
-    <v-data-table :headers="headers" :items="posts" :items-per-page="5" :sort-by="[{ key: 'id', order: 'asc' }]"
-      class="elevation-1" @click:row="serverPage">
-      <template v-slot:top>
-        <v-toolbar flat>
-          <v-toolbar-title>
-            Post List
-            <span v-if="tagname" class="text-body-1 font-italic ml-3">(with {{ tagname }} tagged)</span>
-          </v-toolbar-title>
-          <v-divider class="mx-4" inset vertical></v-divider>
-          <v-spacer></v-spacer>
-          <v-btn variant="tonal" color="blue-darken-4" @click.stop="dialogOpen('create', {})">
-            New post
-          </v-btn>
-        </v-toolbar>
-      </template>
-      <template v-slot:item.actions="{ item }">
-        <v-icon size="small" class="me-2" @click.stop="dialogOpen('update', item.raw)">
-          mdi-pencil
-        </v-icon>
-        <v-icon size="small" @click.stop="deletePost(item.raw)">
-          mdi-delete
-        </v-icon>
-      </template>
-      <template v-slot:no-data>
-        <v-btn color="grey">
-          No data exist
-        </v-btn>
-      </template>
-    </v-data-table>
-
-    <v-dialog v-model="dialog" max-width="800px">
-      <v-card>
-        <v-toolbar dark color="blue-lighten-4">
-          <v-toolbar-title>{{ formTitle }}</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-btn size="small" icon dark @click="cancel">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-        </v-toolbar>
-        <v-card-text>
-          <v-form id="post-form" ref="postForm">
-            <v-container>
-              <v-row>
-                <v-col cols="12" sm="2">
-                  <v-text-field disabled variant="solo" label="id" v-model="editedItem.id"></v-text-field>
-                </v-col>
-                <v-col cols="12" sm="10">
-                  <v-text-field variant="solo" label="title*" required v-model="editedItem.title"></v-text-field>
-                </v-col>
-                <v-col cols="12" sm="2">
-                  <v-text-field disabled variant="solo" label="owner" v-model="editedItem.owner"></v-text-field>
-                </v-col>
-                <v-col cols="12" sm="10">
-                  <v-text-field variant="solo" label="description" v-model="editedItem.description"></v-text-field>
-                </v-col>
-                <v-col cols="12">
-                  <v-textarea label="content" variant="solo" v-model="editedItem.content"></v-textarea>
-                  <v-text-field variant="solo" label="tags" hint="comma separated tag names" persistent-hint
-                   v-model="editedItem.tags"></v-text-field>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-form>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="grey-lighten-1" variant="outlined" @click="cancel">
-            cancel
-          </v-btn>
-          <v-btn class="me-6" color="blue-darken-1" variant="outlined" @click="save">
-            save
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-
+  <v-container style="width:75%">
+    <v-row class="mb-6" style="height: 200px;" align="center" justify="center">
+      <v-col cols="12" lg="12" align="center" class="mt-10 text-h3">
+        <span>Post</span>
+      </v-col>
+      <!-- category -->
+      <v-col cols="12" lg="12" align="center">
+        <v-btn variant="text">new</v-btn>
+        <v-btn variant="text">Review</v-btn>
+        <v-btn variant="text">log</v-btn>
+        <v-btn variant="text">project</v-btn>
+      </v-col>
+    </v-row>
+    <v-row >
+      <v-col cols="12" class="text-subtitle-2">
+        <span>category or tag</span>
+      </v-col>
+      <v-col
+        v-for="post in posts" :key="post.id"
+        xs="12"
+        sm="6"        
+        lg="3"
+      >
+        <v-card 
+        elevation="0" 
+        density="compact" 
+        @click="serverPage(post.id)" 
+        >
+          <v-img
+            :src="post.image"
+            class="rounded-lg"
+          ></v-img>
+          <v-card-subtitle class="mt-2"> {{ post.modify_dt }} </v-card-subtitle>
+          <div>
+            <v-card-title class="d-inline-flex"> {{ post.title }} </v-card-title>
+          </div>
+          <div>
+            <v-card-text class="d-inline-flex"> {{ post.description }} </v-card-text>
+          </div>
+          <div>
+            <v-chip v-for="(tag, index) in post.tags" :key="index" class="ma-1" size="x-small"> {{ tag }} </v-chip>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-import axios from 'axios';
-import { user } from './globals.js';
+import axios from "axios";
+import { user } from "./globals.js";
 
 export default {
   setup() {
-    return { user }
+    return { user };
   },
 
   data: () => ({
@@ -104,11 +72,11 @@ export default {
       { title: "Actions", key: "actions", sortable: false },
     ],
     posts: [],
+    cateList: [],
     tagname: "",
     editedIndex: -1,
     editedItem: {},
     actionKind: "",
-
 
     dialogDelete: false,
   }),
@@ -152,6 +120,22 @@ export default {
       console.log("async.2 in fetchPostList()...", this.user);
     },
 
+    // fetchCateTagList() {
+    //     console.log("fetchCateTagList()...");
+
+    //     axios
+    //       .get(`/api/catetag`)
+    //       .then((res) => {
+    //         console.log("FETCH CATE-TAG-LIST GET RES", res);
+    //         this.cateList = res.data.cateList;
+    //         // this.tagList = res.data.tagList;
+    //       })
+    //       .catch((err) => {
+    //         console.log("FETCH CATE-TAG-LIST GET ERR.RESPONSE", err.response);
+    //         alert(`${err.response.status} ${err.response.statusText}`);
+    //       });
+    //   },
+
     dialogOpen(actionKind, item) {
       console.log("dialogOpen()...", actionKind, item);
       if (this.user.username === "Anonymous") {
@@ -184,75 +168,16 @@ export default {
 
     reset() {
       console.log("reset()...");
-      document.getElementById('post-form').reset();
+      document.getElementById("post-form").reset();
     },
 
-    serverPage(e, { item }) {
+    serverPage(item) {
       console.log("serverPage()...", item);
-      location.href = `/blog/post_detail.html?id=${item.value}`;
-    },
-
-    createPost() {
-      console.log("createPost()...", this.editedItem);
-      const postData = new FormData();
-      for (var key in this.editedItem) {
-        postData.append(key, this.editedItem[key]);
-      }
-
-      axios
-        .post("/api/post/create/", postData)
-        .then((res) => {
-          console.log("CREATE POST POST RES", res);
-          this.posts.push(res.data);
-        })
-        .catch((err) => {
-          console.log("CREATE POST POST ERR.RESPONSE", err.response);
-          alert(err.response.status + " " + err.response.statusText);
-        });
-    },
-
-    updatePost() {
-      console.log("updatePost()...", this.editedItem);
-      const postData = new FormData();
-      for (var key in this.editedItem) {
-        postData.append(key, this.editedItem[key]);
-      }
-      postData.set("owner", this.user.userid);
-      axios
-        .post(`/api/post/${this.editedItem.id}/update/`, postData)
-        .then((res) => {
-          console.log("UPDATE POST POST RES", res);
-          this.posts.splice(this.editedIndex, 1, res.data);
-        })
-        .catch((err) => {
-          console.log("UPDATE POST POST ERR.RESPONSE", err.response);
-          alert(err.response.status + " " + err.response.statusText);
-        });
-    },
-
-    deletePost(item) {
-      console.log("deletePost()...", item, this.user);
-      if (this.user.username === "Anonymous") {
-        alert("Please login first !");
-        return;
-      }
-
-      if (!confirm("Are you sure to delete ?")) return;
-      axios
-        .delete(`/api/post/${item.id}/delete/`)
-        .then((res) => {
-          console.log("DELETE POST DELETE RES", res);
-          const index = this.posts.indexOf(item);
-          this.posts.splice(index, 1);
-        })
-        .catch((err) => {
-          console.log("DELETE POST DELETE ERR.RESPONSE", err.response);
-          alert(err.response.status + " " + err.response.statusText);
-        });
+      location.href = `/blog/post_detail.html?id=${item}`;
     },
 
   },
-}
+};
 </script>
 
 <style scoped>
