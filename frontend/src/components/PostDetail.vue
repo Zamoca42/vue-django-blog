e9c058386e008623af50591c2188504ea1da6e93
Zamoca <suntail2002@naver.com>
2023-03-02 20:29:52 +0900

    refactor: Update detail components

--- frontend/src/components/PostDetail.vue
+++ frontend/src/components/PostDetail.vue
@@ -1,43 +1,55 @@
 <template>
-  <v-container>
+  <v-container style="width:75%">
     <v-row align="center" justify="center">
-      <v-col cols="12" lg="10">
-        <h1>{{ post.title }}</h1>
-        <p>{{ post.modify_dt }}, written by {{ post.owner }}</p>
+      <v-col cols="12" lg="10" class="mt-10 text-center">
+        <p class="mywrap text-h4">{{ post.title }}</p>
+        <p class="text-disabled">
+          <span>{{ post.category }}</span>
+          <span> | </span>
+          <span>{{ post.modify_dt }}</span>
+          <span> | </span>
+          <a
+            class="text-disabled text-decoration-none"
+            href="mailto:suntail93@gmail.com"
+          >
+            {{ post.owner }}
+          </a>
+        </p>
       </v-col>
     </v-row>
-
-    <v-row align="start" justify="center">
-      <v-col cols="12" sm="8" lg="7">
-        <v-card class="pa-2" outlined tile>
-          <p style="white-space: pre-wrap">{{ post.content }}</p>
-          <div>
-            <strong>TAGS:</strong>
-            <v-chip class="ma-2 my-hover" outlined v-for="(tag, index) in post.tags" :key="index"
-              @click="serverPage(tag)">{{ tag }}</v-chip>
+    <v-row align="start" justify="center" >
+      <v-col cols="12" sm="10" lg="10">
+        <v-card class="pa-2" elevation="0">
+          <p class="mywrap" v-html="post.content"></p>
+          <div class="mt-5">
+            <strong class="text-disabled">TAGS:</strong>
+            <v-chip
+              class="ma-2 text-disabled"
+              v-for="(tag, index) in post.tags"
+              :key="index"
+              size="small"
+              @click="serverPage(tag)"
+              >{{ tag }}
+            </v-chip>
           </div>
         </v-card>
       </v-col>
-      <v-col cols="12" sm="4" lg="3">
-        <v-card class="pa-2 mb-5" tile>
-          <p>prev post</p>
-          <h2 v-if="post.prev" @click="fetchPostDetail(post.prev.id)" class="my-hover">
-            {{ post.prev.title }}
-          </h2>
+    </v-row>
+    <v-row align="center" justify="center">
+      <v-col cols="6" sm="5" lg="5">
+        <v-card elevation="1" class="pa-2 mb-5" height="65px"
+        v-if="post.prev" @click="fetchPostDetail(post.prev.id)" 
+        tile hover>
+          <p class="text-disabled"> &lt; prev </p>
+          <p class="myword" v-html="post.prev.title"></p>
         </v-card>
-        <v-card class="pa-2 mb-5" tile>
-          <p>next post</p>
-          <h2 v-if="post.next" @click="fetchPostDetail(post.next.id)" class="my-hover">
-            {{ post.next.title }}
-          </h2>
-        </v-card>
-        <v-card class="pa-2" tile>
-          <h4>Tag cloud</h4>
-          <v-chip v-for="(tag, index) in tagCloud" :key="index" @click="serverPage(tag.name)" class="ma-2 my-hover"
-            :color="tag.chipColor" ttext-color="white">
-            <v-avatar :color="tag.avatarColor" size="x-small" class="me-2">{{ tag.count }}</v-avatar>
-            {{ tag.name }}
-          </v-chip>
+      </v-col>
+      <v-col cols="6" sm="5" lg="5" class="text-right">
+        <v-card elevation="1" class="pa-2 mb-5" height="65px"
+        v-if="post.next" @click="fetchPostDetail(post.next.id)"
+        tile hover>
+        <p class="text-disabled"> next &gt; </p>
+        <p class="myword" v-html="post.next.title"></p>
         </v-card>
       </v-col>
     </v-row>
@@ -50,15 +62,13 @@ import axios from "axios";
 export default {
   data: () => ({
     post: {},
-    tagCloud: [],
   }),
 
   created() {
     console.log("created()...");
     const params = new URL(location).searchParams;
-    const postId = params.get('id');
+    const postId = params.get("id");
     this.fetchPostDetail(postId);
-    this.fetchTagCloud();
   },
 
   methods: {
@@ -73,33 +83,7 @@ export default {
         .catch((err) => {
           console.log("POST DETAIL GET ERR.RESPONSE", err.response);
           alert(err.response.status + " " + err.response.statusText);
-        });
-    },
-
-    fetchTagCloud() {
-      console.log("fetchTagCloud()...");
-      axios
-        .get("/api/tag/cloud/")
-        .then((res) => {
-          console.log("TAG CLOUD GET RES", res);
-          this.tagCloud = res.data;
-          // tag.weight
-          this.tagCloud.forEach((element) => {
-            if (element.weight === 3) {
-              element.chipColor = "green-accent-4";
-              element.avatarColor = "green-lighten-1";
-            } else if (element.weight === 2) {
-              element.chipColor = "cyan-accent-4";
-              element.avatarColor = "blue-lighten-4";
-            } else if (element.weight === 1) {
-              element.chipColor = "grey-darken-1";
-              element.avatarColor = "grey-lighten-1";
-            }
-          });
-        })
-        .catch((err) => {
-          console.log("TAG CLOUD GET ERR.RESPONSE", err.response);
-          alert(err.response.status + " " + err.response.statusText);
+          self.location = '/';
         });
     },
 
@@ -112,8 +96,13 @@ export default {
 </script>
 
 <style scoped>
-.my-hover:hover {
-  cursor: pointer;
-  font-style: italic;
+.mywrap {
+  word-break: keep-all;
+  white-space: pre-wrap;
+}
+.myword {
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
 }
 </style>
