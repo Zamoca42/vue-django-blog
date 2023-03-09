f0b72dc0d30a5dbfd2186cb4e6c68b544e74bf53
Zamoca <suntail2002@naver.com>
2023-03-09 16:56:26 +0900

    chore: Update queryset tag

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
-
 from api.models import Post, Category
 from django.db import models
 
+# 'tag_list',
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'category', 'title', 'image', 'modify_dt', 'tag_list')
-   
+    list_display = ('id', 'category', 'title', 'image', 'modify_dt',)
 
     def get_queryset(self, request):
-        return super().get_queryset(request).prefetch_related('tags')
+        return super().get_queryset(request)
 
     def tag_list(self, obj):
         return u", ".join(o.name for o in obj.tags.all())
