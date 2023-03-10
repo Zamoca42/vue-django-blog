a35b6030e90a06f1396eec1331f0755513e9c926
Zamoca <suntail2002@naver.com>
2023-03-10 17:01:31 +0900

    feat: add default image

--- frontend/src/components/PostList.vue
+++ frontend/src/components/PostList.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container style="width: 75%">
+  <v-container fluid style="width: 1200px">
     <v-row class="mb-6" style="height: 200px" align="center" justify="center">
       <v-col cols="12" lg="10" align="center" class="mt-10 text-h3">
         <span>Post</span>
@@ -47,12 +47,22 @@
           :ripple="false"
           class="rounded-xl fill-height"
         >
-          <v-img
-            :src="post.image"
-            class="rounded-xl"
-            style="height: 150px"
-            cover
-          ></v-img>
+          <template v-if="post.image === null">
+            <v-img
+              :src="defaultImageUrl"
+              class="rounded-xl"
+              style="height: 150px"
+              cover
+            ></v-img>
+          </template>
+          <template v-else>
+            <v-img
+              :src="post.image"
+              class="rounded-xl"
+              style="height: 150px"
+              cover
+            ></v-img>
+          </template>
           <v-card-text>
             <v-row>
               <v-col sm="10" cols="12" class="text-sm-left text-center">
@@ -91,6 +101,7 @@
 <script>
 import axios from "axios";
 // import { user } from "./globals.js";
+// import image from 'https://picsum.photos/900/300?grayscale'
 
 export default {
   setup() {
@@ -104,9 +115,14 @@ export default {
     category: "",
     pageCnt: 1,
     curPage: 1,
+    // image: "",
+    defaultImageUrl: "https://picsum.photos/500/150?grayscale",
   }),
 
-  computed: {},
+  computed: {
+    // imageSrc() {
+    //   return this.image || this.defaultImageUrl;
+  },
 
   created() {
     // console.log("created(PostList.vue)...", this.user);
@@ -126,24 +142,22 @@ export default {
       console.log("fetchPostList()...", this.tagname);
 
       let getUrl = "";
-      if (this.tagname)
-        getUrl = `/api2/post/?tagname=${this.tagname}`;
-      else if (this.category)
-        getUrl = `/api2/post/?category=${this.category}`;
+      if (this.tagname) getUrl = `/api2/post/?tagname=${this.tagname}`;
+      else if (this.category) getUrl = `/api2/post/?category=${this.category}`;
       else getUrl = `/api2/post/`;
 
       axios
-      .get(getUrl)
-      .then((res) => {
-        console.log("FETCH POSTLIST GET RES", res);
-        this.postList = res.data.postList;
-        this.pageCnt = res.data.pageCnt;
-        this.curPage = res.data.curPage;
-      })
-      .catch((err) => {
-        console.log("POST LIST GET ERR.RESPONSE", err.response);
-        alert(err.response.status + " " + err.response.statusText);
-      });
+        .get(getUrl)
+        .then((res) => {
+          console.log("FETCH POSTLIST GET RES", res);
+          this.postList = res.data.postList;
+          this.pageCnt = res.data.pageCnt;
+          this.curPage = res.data.curPage;
+        })
+        .catch((err) => {
+          console.log("POST LIST GET ERR.RESPONSE", err.response);
+          alert(err.response.status + " " + err.response.statusText);
+        });
       // try {
       //   const res = await axios.get(getUrl);
       //   console.log("POST LIST GET RES", res);
@@ -158,8 +172,8 @@ export default {
     },
 
     serverPage(item) {
-        console.log("serverPage()...", item);
-        location.href = `/blog/post_detail.html?id=${item}`;
+      console.log("serverPage()...", item);
+      location.href = `/blog/post_detail.html?id=${item}`;
     },
   },
 };
