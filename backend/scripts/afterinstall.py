8d8498b05684d400530946507dda7171067a652e
Zamoca <suntail2002@naver.com>
2023-03-13 02:16:11 +0900

    feat: Update aws ec2 codedeploy

---
+++ backend/scripts/afterinstall.py
@@ -0,0 +1,11 @@
+print("after")
+import os
+
+os.system('sudo chmod -R gu+rwx /home/ubuntu/backend')
+
+os.system('python3 -m venv /home/ubuntu/backend/venv')
+os.system('sudo chown -R ubuntu.ubuntu /home/ubuntu/backend')
+os.system('sudo chmod -R gu+rwx /home/ubuntu/backend')
+os.system(
+    '. /home/ubuntu/backend/venv/bin/activate && pip install --upgrade pip && pip install -r /home/ubuntu/backend/requirements/prod.txt && python /home/ubuntu/backend/manage.py migrate')
+    
---
+++ backend/scripts/afterinstall.sh
@@ -0,0 +1,1 @@
+python3 /home/ubuntu/backend/scripts/afterInstall.py 1> /home/ubuntu/after.log 2> /home/ubuntu/after.err
