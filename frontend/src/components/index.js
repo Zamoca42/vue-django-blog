5b1228db7b1466dbe398cb445ea5a043719054bb
Zamoca <suntail2002@naver.com>
2023-03-14 22:51:37 +0900

    refactor: Update instance url

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://43.201.163.241', // Replace with your Django server's URL
+  baseURL: 'https://server.zamoca.space', // Replace with your Django server's URL
   timeout: 10000 // Set a timeout if needed
 });
 
