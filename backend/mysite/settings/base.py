5533cdd8b5053c86f61f5fcbb2d983083569fe43
Zamoca <suntail2002@naver.com>
2023-03-24 22:55:53 +0900

    feat: Added DRF throttling

--- backend/mysite/settings/base.py
+++ backend/mysite/settings/base.py
@@ -203,6 +203,14 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '50/day',
+        'user': '1000/day'
+    }
 }
 
 # TAGGIT_CASE_INSENSITIVE = True
