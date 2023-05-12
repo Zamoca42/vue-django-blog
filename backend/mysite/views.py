981a8b42713105cc2dbb197d20ba825338c4f5a9
Zamoca <suntail2002@naver.com>
2023-05-12 16:57:43 +0900

    feat: robots.txt 추가

--- backend/mysite/views.py
+++ backend/mysite/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 from django.views.decorators.http import require_GET
+
 @require_GET
 def robots_txt(request):
     lines = [
