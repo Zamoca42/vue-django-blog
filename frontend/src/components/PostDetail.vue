c0a78210c626b1c6414aa38e1c97d5e301eafe3a
Zamoca <suntail2002@naver.com>
2023-05-25 12:26:49 +0900

    fix: 포스트 이외의 접근 시 예외 처리

--- frontend/src/components/PostDetail.vue
+++ frontend/src/components/PostDetail.vue
@@ -8,10 +8,7 @@
           <span> | </span>
           <span>{{ post.create_dt }}</span>
           <span> | </span>
-          <a
-            class="text-disabled text-decoration-none"
-            href="mailto:suntail93@gmail.com"
-          >
+          <a class="text-disabled text-decoration-none" href="mailto:suntail93@gmail.com">
             {{ post.owner }}
           </a>
         </p>
@@ -24,18 +21,11 @@
 
           <div class="mt-5">
             <div>
-              <strong class="text-disabled"
-                >Last Modified at {{ post.modify_dt }}</strong
-              >
+              <strong class="text-disabled">Last Modified at {{ post.modify_dt }}</strong>
             </div>
             <strong class="text-disabled">TAGS:</strong>
-            <v-chip
-              class="ma-2 text-disabled"
-              v-for="(tag, index) in post.tags"
-              :key="index"
-              size="small"
-              @click="serverPage(tag)"
-              >{{ tag }}
+            <v-chip class="ma-2 text-disabled" v-for="(tag, index) in post.tags" :key="index" size="small"
+              @click="serverPage(tag)">{{ tag }}
             </v-chip>
           </div>
         </v-card>
@@ -43,29 +33,15 @@
     </v-row>
     <v-row align="center" justify="center">
       <v-col cols="6" sm="5" lg="5">
-        <v-card
-          elevation="1"
-          class="pa-2 mb-5"
-          height="65px"
-          v-if="prev"
-          @click="$router.push({ name: 'Detail', params: { id: prev.id } })"
-          tile
-          hover
-        >
+        <v-card elevation="1" class="pa-2 mb-5" height="65px" v-if="prev"
+          @click="$router.push({ name: 'Detail', params: { id: prev.id } })" tile hover>
           <p class="text-disabled">&lt; prev</p>
           <p class="myword ml-2" v-html="prev.title"></p>
         </v-card>
       </v-col>
       <v-col cols="6" sm="5" lg="5" class="text-right">
-        <v-card
-          elevation="1"
-          class="pa-2 mb-5"
-          height="65px"
-          v-if="next"
-          @click="$router.push({ name: 'Detail', params: { id: next.id } })"
-          tile
-          hover
-        >
+        <v-card elevation="1" class="pa-2 mb-5" height="65px" v-if="next"
+          @click="$router.push({ name: 'Detail', params: { id: next.id } })" tile hover>
           <p class="text-disabled">next &gt;</p>
           <p class="myword me-2" v-html="next.title"></p>
         </v-card>
@@ -144,14 +120,24 @@ export default {
 
   methods: {
     async fetchPostData(id) {
-      const res = await axios.get(`/api2/post/${id}/`);
-      this.post = res.data.post;
-      this.markedContent = this.post.content;
-      this.prev = res.data.prevPost;
-      this.next = res.data.nextPost;
-      this.$nextTick(() => {
-        window.scrollTo(0, 0);
-      });
+      try {
+        const res = await axios.get(`/api2/post/${id}/`);
+        this.post = res.data.post;
+        this.markedContent = this.post.content;
+        this.prev = res.data.prevPost;
+        this.next = res.data.nextPost;
+        this.$nextTick(() => {
+          window.scrollTo(0, 0);
+        });
+      } catch (error) {
+        if (error.response && error.response.status === 404) {
+          // 포스트가 없는 경우 404 페이지로 리다이렉트합니다.
+          this.$router.push('/not-found');
+        } else {
+          // 다른 오류를 처리합니다.
+          console.error(error);
+        }
+      }
     },
 
     async handleError(error, next) {
